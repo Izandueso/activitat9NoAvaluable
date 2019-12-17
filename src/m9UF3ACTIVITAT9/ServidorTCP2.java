d2aa7f2f760ea172249b0dc44d1eeeeae4c2c6cf
Alumne <Alumne@104-PC12>
2019-12-17 17:49:21 +0100

    LastCommit

--- src/m9UF3ACTIVITAT9/ServidorTCP2.java
+++ src/m9UF3ACTIVITAT9/ServidorTCP2.java
@@ -4,7 +4,16 @@ import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 
-public class ServidorTCP2 {
+public class ServidorTCP2 implements Runnable{
+	String cadena;
+	ServerSocket server;
+	int numClient;
+	Socket client;
+	public ServidorTCP2(Socket cliente, ServerSocket server){
+		this.server = server;
+		this.numClient++;
+		this.client = cliente;
+	}
 
 	public static void main (String[] args) throws Exception {
 
@@ -24,7 +33,10 @@ public class ServidorTCP2 {
 
 		// Determinem les vegades que es conectaran els clients
 		for (int i = 0; i < numClients; i++) {
-
+			
+			Runnable[] arrayRunnable = new Runnable[10];
+			Thread[]arrayThread = new Thread[10];
+			
 			Socket clientConnectat = null;
 			PrintWriter fsortida = null;
 			BufferedReader fentrada = null;
@@ -60,6 +72,46 @@ public class ServidorTCP2 {
 		//		fsortida.close();
 		//		clientConnectat.close();
 		servidor.close();
+
+	}
+
+	@Override
+	public void run() {
+		try {
+			
+			//FLUX DE SORTIDA AL CLIENT
+			
+			PrintWriter fsortida = new PrintWriter(this.client.getOutputStream(), true);
+			
+			//FLUX D'ENTRADA DEL CLIENT
+			
+			BufferedReader fentrada = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
+
+			fsortida.println("Client: " + this.numClient);
+			
+			while ((cadena = fentrada.readLine()) != null) {
+				
+				fsortida.println(cadena);
+				System.out.println("Rebent: "+cadena);
+				if (cadena.equals("*"))
+					
+				break;
+				
+			}
+
+			//TANCAR STREAMS I SOCKETS
+			System.out.println("Tancant connexió... ");
+			
+			fsortida.close();
+			fentrada.close();
+			this.server.close();
+			this.client.close();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+			
+		}
 
 	}
 
